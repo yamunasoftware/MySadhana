a4568ea9826f533177381c22d4e56ce3fd83f44d
Yamuna Software Solutions - Sushen Kolakaleti <yamunasoftware18@gmail.com>
2022-02-06 02:07:06 -0500

    Add files via upload

--- src/JS/cloud.js
+++ src/JS/cloud.js
@@ -536,7 +536,7 @@ function highlightDates(string, dates) {
   //Loops through Array:
   mainLoop: while (turns < dates.length) {
     //Sets the String:
-    var value = new RegExp(data[turns], "g");
+    var value = data[turns];
     localString = localString.replace(value, "<mark>" + value + "</mark>");
     
     turns++;
