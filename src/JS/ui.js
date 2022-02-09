4d4efc90d7f955ca7743133425dc4e81520a4415
Yamuna Software Solutions - Sushen Kolakaleti <yamunasoftware18@gmail.com>
2022-02-08 20:02:28 -0500

    Add files via upload

--- src/JS/ui.js
+++ src/JS/ui.js
@@ -156,7 +156,6 @@ function showNavigation() {
 //Close Navigation Function:
 function closeNavigation() {
   //Closes the Element:
-  document.getElementById('navigation').style.visibility = "hidden";
   document.getElementById('navigation').classList.remove('ease-in');
   document.getElementById('navigation').classList.add('ease-out');
 }
