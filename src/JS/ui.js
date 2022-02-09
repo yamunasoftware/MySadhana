3adb9c203ab30c7ebbc095b0de01960511627216
Yamuna Software Solutions - Sushen Kolakaleti <yamunasoftware18@gmail.com>
2022-02-08 20:17:32 -0500

    Add files via upload

--- src/JS/ui.js
+++ src/JS/ui.js
@@ -149,7 +149,6 @@ function showError(error) {
 function showNavigation() {
   //Displays the Element:
   document.getElementById('navigation').style.visibility = "visible";
-  document.getElementById('navigation').classList.remove('ease-out');
   document.getElementById('navigation').classList.add('ease-in');
 }
 
@@ -157,6 +156,4 @@ function showNavigation() {
 function closeNavigation() {
   //Closes the Element:
   document.getElementById('navigation').style.visibility = "hidden";
-  document.getElementById('navigation').classList.remove('ease-in');
-  document.getElementById('navigation').classList.add('ease-out');
 }
