53d924f9a92ded4bc7d56251fcba50de32b0f737
Yamuna Software Solutions - Sushen Kolakaleti <yamunasoftware18@gmail.com>
2022-03-02 01:47:45 -0500

    Add files via upload

--- src/JS/cloud.js
+++ src/JS/cloud.js
@@ -214,6 +214,8 @@ function saveNote() {
     data = getCacheData(dataID, true);
     data[saveIndex] = document.getElementById('text-area').innerHTML;
     data[saveIndex] = data[saveIndex].replace(/(\r\n|\n|\r)/gm, "<br>");
+    data[saveIndex] = data[saveIndex].replace("<div>", "");
+    data[saveIndex] = data[saveIndex].replace("</div>", "");
 
     //Saves the Data:
     setCacheData(dataID, data, true);
